ea9665bfa9200a729d901a74ce0db370406c7a78
husj <husj@gtintel.cn>
2016-09-26 14:15:23 +0800

    add

---
+++ testsub1/src/main/java/test/rule/Test.java
@@ -0,0 +1,45 @@
+/**************************************************************************************** 
+           南京国通智能科技有限公司  (优秀是习惯 规范是能力 交付是品德 质量是生命)           
+ ****************************************************************************************/
+package test.rule;
+
+/**
+ * <Description> <br>
+ * 
+ * @author Administrator<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2016年9月26日 <br>
+ */
+
+public class Test {
+
+    public String test1(String a) {
+        return "";
+    }
+
+    /**
+     * Description: <br>
+     * 
+     * @author Administrator<br>
+     * @taskId <br>
+     * @param a
+     * @return <br>
+     */
+    public String test2(String a) {
+        return "";
+    }
+
+    /**
+     * Description: <br>
+     * 
+     * @author Administrator<br>
+     * @taskId <br>
+     * @param args <br>
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
